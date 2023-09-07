2d028a61691e8b456b2a0d6b22d86437b3606c86
Kaikei-e <e_kaikei@protonmail.com>
2023-09-07 17:15:10 +0900

    creating the caffeine input form

--- app/components/CaffeineInput.tsx
+++ app/components/CaffeineInput.tsx
@@ -1,13 +1,39 @@
 'use client';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useForm} from 'react-hook-form';
 
 export const CaffeineInput: React.FC = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
+    const [currentDateTime, setCurrentDateTime] = useState<string>('');
     const onSubmit = (data: any) => console.log(data);
+
+    useEffect(() => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const dateTimeString = `${year}-${month}-${day}T${hours}:${minutes}`;
+
+        setCurrentDateTime(dateTimeString);
+    }, []);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center p-5 bg-gray-100 rounded-lg">
+            <div className="flex flex-col mb-5">
+                <label htmlFor="caffeineTime" className="text-lg font-bold mb-2">Time Consumed:</label>
+                <input
+                    type="datetime-local"
+                    id="caffeineTime"
+                    {...register('caffeineTime', {
+                        required: "Required",
+                    })}
+                    value={currentDateTime}
+                    className={`p-2 rounded border ${errors.caffeineTime ? 'border-red-500' : 'border-gray-300'}`}
+                />
+                {errors.caffeineTime && <span className="text-red-500 text-xs">{errors.caffeineTime.message as string}</span>}
+            </div>
             <div className="flex flex-col mb-5">
                 <label htmlFor="caffeineAmount" className="text-lg font-bold mb-2">Caffeine Amount (mg):</label>
                 <input
@@ -25,6 +51,7 @@ export const CaffeineInput: React.FC = () => {
                 {errors.caffeineAmount &&
                     <span className="text-red-500 text-xs">{errors.caffeineAmount.message as string}</span>}
             </div>
+
             <button type="submit"
                     className="px-4 py-2 rounded text-white bg-indigo-400 hover:bg-blue-300 hover:text-black">Calculate
             </button>
